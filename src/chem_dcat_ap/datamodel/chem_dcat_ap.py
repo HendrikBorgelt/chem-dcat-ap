fb6168137fd9e77bd329b3939fae04593beba652
HendrikBorgelt <84382772+HendrikBorgelt@users.noreply.github.com>
2026-06-01 14:14:41 +0200

    Added documentation for working with data and open SHACL
    Modified the SHACL cli commands to only have open classes and added a documentation page for working with data, explaining current issues and limitations also regarding curies and uris.

--- src/chem_dcat_ap/datamodel/chem_dcat_ap.py
+++ src/chem_dcat_ap/datamodel/chem_dcat_ap.py
@@ -1,5 +1,5 @@
 # Auto generated from chem_dcat_ap.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-06-01T13:29:20
+# Generation date: 2026-06-01T13:39:54
 # Schema: chem-dcat-ap
 #
 # id: https://w3id.org/nfdi-de/dcat-ap-plus/chemistry/
